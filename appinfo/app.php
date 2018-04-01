32aaa94fb7f01c6725be261299466a4e65b948bf
Aaron Scheiner <blue@aquarat.za.net>
2018-04-01 23:51:31 +0200

    UNTESTED: Added Sony ARW Raw as a handled file type.

--- appinfo/app.php
+++ appinfo/app.php
@@ -11,7 +11,9 @@ $mimeTypeLoader = \OC::$server->getMimeTypeLoader();
 // Register custom mimetype we can hook in the frontend
 $mimeTypeDetector->getAllMappings();
 $mimeTypeDetector->registerType('indd', 'image/x-indesign', 'application/x-indesign');
+$mimeTypeDetector->registerType('arw', 'image/x-sony-arw', 'image/x-sony-arw');
 
 $previewManager = $container->getServer()->query('PreviewManager');
 $previewManager->registerProvider('/image\/x-dcraw/', function() { return new \OCA\CameraRawPreviews\RawPreview; });
 $previewManager->registerProvider('/image\/x-indesign/', function() { return new \OCA\CameraRawPreviews\IndesignPreview; });
+$previewManager->registerProvider('/image\/x-sony-arw/', function() { return new \OCA\CameraRawPreviews\RawPreview; });
